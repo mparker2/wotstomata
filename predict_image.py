2f4b7598a786999abbb2ccf942c692e82514c789
mparker2 <mparker2@sheffield.ac.uk>
2017-12-30 11:08:08 +0000

    add roi generation to predict_image.py

--- predict_image.py
+++ predict_image.py
@@ -10,6 +10,9 @@ import click
 import h5py
 
 from hourglass import load_model
+from raster_to_shapes import (write_geoms_as_rois,
+                              edgemap_segments_to_polygons,
+                              heatmap_peaks_to_points)
 
 
 def read_multipage_tiff(path):
@@ -33,13 +36,10 @@ def threshold_image(img):
     return img
 
 
-def _predict_img(img_fn, model, step_size=128):
-    zstack = read_multipage_tiff(img_fn)
+def _predict_img(zstack, model, step_size=128):
     assert zstack.shape[-1] == 3
     heatmap = []
     segments = []
-    stomatal_density = []
-    all_cell_density = []
     blur_levels = []
     zmax, imax, jmax, _ = zstack.shape
     for z in range(zmax):
@@ -58,17 +58,13 @@ def _predict_img(img_fn, model, step_size=128):
                 slic = padded
             X.append(slic)
             pos.append((i, j))
-        hm_preds, seg_preds, stomata, all_cells, blur = model.predict(
+        hm_preds, seg_preds, *_, blur = model.predict(
             np.asarray(X))[-5:]
         z_hm = np.zeros((imax, jmax), dtype='f')
         z_segs = z_hm.copy()
-        z_density = z_hm.copy()
-        z_all_cells = z_hm.copy()
         z_blur = z_hm.copy()
         tot_in_pos = z_hm.copy()
-        for (i, j), h, s, st, a, b in zip(pos, hm_preds, seg_preds,
-                                          stomata.ravel(), all_cells.ravel(),
-                                          blur.ravel()):
+        for (i, j), h, s, b in zip(pos, hm_preds, seg_preds, blur.ravel()):
             h = resize(threshold_image(h).reshape(h.shape[:-1]),
                        (256, 256), preserve_range=True)
             s = resize(threshold_image(s).reshape(s.shape[:-1]),
@@ -77,20 +73,12 @@ def _predict_img(img_fn, model, step_size=128):
                                               :min(h.shape[1], jmax - j)]
             z_segs[i: i + 256, j: j + 256] += s[:min(s.shape[0], imax - i),
                                                 :min(s.shape[1], jmax - j)]
-            z_density[i: i + 256, j: j + 256] += st
-            z_all_cells[i: i + 256, j: j + 256] += a
             z_blur[i: i + 256, j: j + 256] += b
             tot_in_pos[i:i + 256, j: j + 256] += 1
         heatmap.append(z_hm / tot_in_pos)
         segments.append(z_segs / tot_in_pos)
-        stomatal_density.append(z_density / tot_in_pos)
-        all_cell_density.append(z_all_cells / tot_in_pos)
         blur_levels.append(z_blur / tot_in_pos)
-    blur_levels = np.asarray(blur_levels)
-    return (np.asarray(heatmap), np.asarray(segments),
-            np.asarray(stomatal_density),
-            np.asarray(all_cell_density),
-            np.asarray(blur_levels))
+    return np.asarray(heatmap), np.asarray(segments), np.asarray(blur_levels)
 
 
 @click.command()
@@ -99,27 +87,55 @@ def _predict_img(img_fn, model, step_size=128):
 @click.option('--arch', required=True, help='json model architecture')
 @click.option('--weights', required=True, help='h5 model weights')
 @click.option('--output', required=False, default=None,
-              help='otuput hdf5 file')
+              help='otuput file basename')
 @click.option('--step-size', default=128)
-def predict_img(img, arch, weights, output, step_size):
+@click.option('--include-inverted', default=True)
+@click.option('--generate-rois', default=True)
+@click.option('--roi-lines/--roi-polys', default=False)
+@click.option('--points-3d/--points-flattened', default=False)
+def predict_img(img, arch, weights, output,
+                step_size, include_inverted,
+                generate_rois, roi_lines, points_3d):
     model = load_model(arch, weights)
     if os.path.isdir(img):
         glob_path = img.rstrip('/') + '/*.tif'
         imgs = glob(glob_path)
-        outputs = [os.path.splitext(i)[0] + '.prediction.h5' for i in imgs]
+        outputs = [os.path.splitext(i)[0] for i in imgs]
     else:
         imgs = [img, ]
         outputs = [output, ]
-    for img, output in zip(imgs, outputs):
+    for img_fn, output in zip(imgs, outputs):
+        img = read_multipage_tiff(img_fn)
         heatmap, segments, density, all_cells, blur = _predict_img(
             img, model, step_size)
         h5_file = h5py.File(output, mode='w')
         h5_file.create_dataset('heatmap', data=heatmap)
         h5_file.create_dataset('segments', data=segments)
-        h5_file.create_dataset('stomatal_density', data=density)
-        h5_file.create_dataset('all_cell_density', data=all_cells)
         h5_file.create_dataset('blur', data=blur)
+        if include_inverted:
+            heatmap_i, segments_i, blur_i = _predict_img(
+                img[:, ::-1, ::-1, :], model, step_size)
+            heatmap_i = heatmap_i[:, ::-1, ::-1]
+            segments_i = segments_i[:, ::-1, ::-1]
+            blur_i = blur_i[:, ::-1, ::-1]
+            h5_file = h5py.File(output + '.prediction.h5', mode='w')
+            h5_file.create_dataset('heatmap_i', data=heatmap_i)
+            h5_file.create_dataset('segments_i', data=segments_i)
+            h5_file.create_dataset('blur_i', data=blur_i)
         h5_file.close()
+        if generate_rois:
+            if include_inverted:
+                heatmap = np.concatenate([heatmap, heatmap_i], axis=0)
+                segments = np.concatenate([segments, segments_i], axis=0)
+                blur = np.concatenate([blur, blur_i], axis=0)
+            i, j = np.indices(segments.shape[1:-1])
+            seg_max = segments[np.argmax(blur, axis=0), i, j]
+            polys = edgemap_segments_to_polygons(seg_max)
+            if not points_3d:
+                heatmap = heatmap[np.argmax(blur, axis=0), i, j]
+            points = heatmap_peaks_to_points(heatmap)
+            write_geoms_as_rois(polys, output + '.segmentedcells.roi')
+            write_geoms_as_rois(points, output + '.stomatapoints.roi')
 
 if __name__ == '__main__':
     predict_img()
